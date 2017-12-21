1ece232dad5c946edb12c7fd3a885ed9c107e63c
francesco.zanoni <francesco.zanoni@nubilaria.com>
2017-12-21 12:18:17 +0100

    added first migrations: students, questions, answers, compilations

---
+++ database/migrations/2017_12_21_110348_create_answers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text')->nullable(false);
+            $table->enum('code', ['A', 'B', 'C', 'D'])->nullable(false);
+            $table->unsignedInteger('question_id')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
---
+++ database/migrations/2017_12_21_110432_create_compilations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompilationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compilations', function (Blueprint $table) {
+            $table->increments('id');
+            // @todo choose table structure: compilations stored vertically or horizontally?
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compilations');
+    }
+}
