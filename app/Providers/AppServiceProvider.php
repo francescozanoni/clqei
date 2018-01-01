7548918c2fa31fcacd3b015a7d56ee2af086eb71
Francesco Zanoni <francescozanoni81@gmail.com>
2018-01-01 18:14:21 +0100

    student: added service that lists countries, names according to locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(App\Services\CountryService::class, function ($app) {
+          // @todo add country list retrieval.
+          return new App\Services\CountryService();
+        });
     }
 }
