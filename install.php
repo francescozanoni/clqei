90f5fcc22bc00dc5d5dd0ce345f9a034826a5248
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2018-01-13 11:07:05 +0100

    localization: added automatic installation steps of Italian language (Laravel's strings) to install.php script

--- install.php
+++ install.php
@@ -75,24 +75,22 @@ copy(
 );
 
 # Language files
-/*
-echo 'Additional language? (e.g.: it) ';
-$additionalLanguage = trim(fgets(STDIN));
+$additionalLanguage = 'it';
 # Function taken from http://www.beliefmedia.com/copy-directory-php
 function beliefmedia_recurse_copy($src, $dst) {
   $dir = @opendir($src);
   if (!file_exists($dst)) @mkdir($dst);
   while (false !== ($file = readdir($dir))) {
       if (( $file != '.' ) && ( $file != '..' )) {
-         if ( is_dir($src . '/' . $file) ) beliefmedia_recurse_copy($src . '/' . $file, $dst . '/' . $file); 
+         if ( is_dir($src . '/' . $file) ) beliefmedia_recurse_copy($src . '/' . $file, $dst . '/' . $file);
          else copy($src . '/' . $file, $dst . '/' . $file);
       }
   }
-  closedir($dir); 
+  closedir($dir);
 }
 $src = __DIR__ . '/vendor/caouecs/laravel-lang/src/' . $additionalLanguage;
 $dst = __DIR__ . '/resources/lang/' . $additionalLanguage;
-beliefmedia_recurse_copy($src, $dst);*/
+beliefmedia_recurse_copy($src, $dst);
 
 echo 'Setting up additional software...' . PHP_EOL;
 
