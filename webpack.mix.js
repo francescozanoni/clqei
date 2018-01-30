b860ae611a7dab6b5487688783989b2494638679
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2018-01-30 10:20:46 +0100

    webpack building: fixed font path

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// setResourceRoot('../') is required to correctly resolve font paths,
+// in case of application reachable by a domain subfolder
+mix.setResourceRoot('../')
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css');
