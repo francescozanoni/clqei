f2e8c9dcb4d6e2822b6856c32f2c5c9bbe839089
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2018-02-09 16:35:15 +0100

    tests: continued student test

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -32,6 +32,8 @@ class StudentTest extends TestCase
 
         $response = $this->actingAs($user)->get(route('compilations.index'));
         $response->assertStatus(200);
+        // Compilation list is not rendered by DataTables, for students.
+        $response->assertDontSee('dataTables');
 
         $response = $this->actingAs($user)->get(route('users.show', ['user' => $user]));
         $response->assertStatus(200);
@@ -51,6 +53,7 @@ class StudentTest extends TestCase
 
         $user = User::students()->first();
 
+        // Students cannot do anything with stage locations.
         $response = $this->actingAs($user)->get(route('locations.index'));
         $response->assertRedirect(route('home'));
         $response = $this->actingAs($user)->get(route('locations.edit', ['location' => Location::first()]));
@@ -62,6 +65,7 @@ class StudentTest extends TestCase
         $response = $this->actingAs($user)->delete(route('locations.destroy', ['location' => Location::first()]));
         $response->assertRedirect(route('home'));
 
+        // Students cannot do anything with stage wards.
         $response = $this->actingAs($user)->get(route('wards.index'));
         $response->assertRedirect(route('home'));
         $response = $this->actingAs($user)->get(route('wards.edit', ['ward' => Ward::first()]));
@@ -73,21 +77,38 @@ class StudentTest extends TestCase
         $response = $this->actingAs($user)->delete(route('wards.destroy', ['ward' => Location::first()]));
         $response->assertRedirect(route('home'));
 
+        // Students cannot see the list of users.
         $response = $this->actingAs($user)->get(route('users.index'));
         $response->assertRedirect(route('home'));
+        // Students cannot see other users' profile page.
         $response = $this->actingAs($user)->get(route('users.show', ['user' => User::administrators()->first()]));
         $response->assertStatus(403);
         $response = $this->actingAs($user)->get(route('users.show', ['user' => User::viewers()->first()]));
         $response->assertStatus(403);
+        // Students cannot delete themselves.
         $response = $this->actingAs($user)->delete(route('users.destroy', ['user' => $user]));
         $response->assertStatus(403);
-
-        // These two assertions are to be updated once user edit logic is implemented.
+        // @todo add test that students cannot delete other students
+        // These two assertions are to be updated and moved to available pages method,
+        // once user edit logic is implemented.
         $response = $this->actingAs($user)->get(route('users.edit', ['user' => $user]));
         $response->assertRedirect(route('home'));
         $response = $this->actingAs($user)->put(route('users.update', ['user' => $user]));
         $response->assertRedirect(route('home'));
 
+        // Pages available only to unauthenticated users.
+        $response = $this->get(route('login'));
+        $response->assertRedirect(route('home'));
+        $response = $this->get(route('register'));
+        $response->assertRedirect(route('home'));
+        $response = $this->get(route('password.request'));
+        $response->assertRedirect(route('home'));
+        $response = $this->get(route('password.reset', ['token' => 'random']));
+        $response->assertRedirect(route('home'));
+
     }
+
+    //@todo add test that a student can see his/her compilations
+    //@todo add test that a student cannot see other students' compilations
 
 }
