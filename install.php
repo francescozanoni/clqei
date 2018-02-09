36f3fc8a49b331732537d3c94b1678a2e7faed80
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2018-02-09 14:30:15 +0100

    install script: improved automation

--- install.php
+++ install.php
@@ -1,24 +1,56 @@
 #!/usr/bin/env php
 <?php
+# #####################################################
 
-$filePathsToCheck = [
+# INSTALLATION OPTIONS
 
-	__DIR__ . '/public/.htaccess',
-
-	__DIR__ . '/database/database.sqlite',
-
-	__DIR__ . '/.env',
-
-    __DIR__ . '/phpunit.xml',
-
-	__DIR__ . '/public/phpliteadmin/phpliteadmin.config.php',
-	__DIR__ . '/public/phpliteadmin/phpliteadmin.config.sample.php',
-	__DIR__ . '/public/phpliteadmin/phpliteadmin.php',
-	__DIR__ . '/public/phpliteadmin/readme.md',
-
+$options = [
+    'application_url' => 'http://localhost',
+    'with_phpliteadmin' => false,
+    'locale' => 'it',
+    'locale_source_path' => __DIR__ . '/vendor/caouecs/laravel-lang/src',
+    'locale_destination_path' => __DIR__ . '/resources/lang',
+    'phpliteadmin_url' => 'https://bitbucket.org/phpliteadmin/public/downloads/phpLiteAdmin_v1-9-7-1.zip'
 ];
 
-// Ensure all previous file paths do not exist.
+$inputOptions = getopt('', ['application_url:', 'with_phpliteadmin', 'locale:']);
+
+if (isset($inputOptions['application_url']) === true) {
+    if (filter_var($inputOptions['application_url'], FILTER_VALIDATE_URL) === false) {
+        die('Invalid application URL' . PHP_EOL);
+    }
+    $options['application_url'] = $inputOptions['application_url'];
+}
+if (array_key_exists('with_phpliteadmin', $inputOptions) === true) {
+    $options['with_phpliteadmin'] = true;
+}
+if (isset($inputOptions['locale']) === true) {
+    // @todo validate locale against locales within folder vendor/caouecs/laravel-lang/src
+    if (preg_match($inputOptions['locale'], '/^[a-z]{2}(\-[A-Z]{2})?$/') !== 1) {
+        die('Invalid locale' . PHP_EOL);
+    }
+    $options['locale'] = $inputOptions['locale'];
+}
+
+# #####################################################
+
+# FILE INEXISTENCE CHECK
+
+$filePathsToCheck = [
+    __DIR__ . '/public/.htaccess',
+    __DIR__ . '/database/database.sqlite',
+    __DIR__ . '/database/test_database.sqlite',
+    __DIR__ . '/.env',
+    __DIR__ . '/phpunit.xml',
+];
+
+if ($options['with_phpliteadmin'] === true) {
+    $filePathsToCheck[] = __DIR__ . '/public/phpliteadmin/phpliteadmin.config.php';
+    $filePathsToCheck[] = __DIR__ . '/public/phpliteadmin/phpliteadmin.config.sample.php';
+    $filePathsToCheck[] = __DIR__ . '/public/phpliteadmin/phpliteadmin.php';
+    $filePathsToCheck[] = __DIR__ . '/public/phpliteadmin/readme.md';
+}
+
 foreach ($filePathsToCheck as $filePath) {
     if (file_exists($filePath) === true) {
         die($filePath . ' already exists' . PHP_EOL);
@@ -27,31 +59,30 @@ foreach ($filePathsToCheck as $filePath) {
 
 # #####################################################
 
-# Other software URLs
-$urls = [
-    'phpliteadmin' => 'https://bitbucket.org/phpliteadmin/public/downloads/phpLiteAdmin_v1-9-7-1.zip',
-];
+# SCRIPT REQUIREMENT CHECK
+
+if ($options['with_phpliteadmin'] === true) {
+    echo 'Checking installation script requirements...' . PHP_EOL;
+    if (extension_loaded('zip') === false) {
+        die('PHP zip extension is required but unavailable. Aborting...' . PHP_EOL);
+    }
+}
 
 # #####################################################
-
-echo 'Checking installation script requirements...' . PHP_EOL;
-
-# Current script dependencies
-if (extension_loaded('zip') === false) {
-    die('PHP zip extension is required but unavailable. Aborting...' . PHP_EOL);
-}
 
 echo 'Setting up directories...' . PHP_EOL;
 
 # Directories writable by the web user
 $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('storage'));
-foreach($iterator as $item) {
+foreach ($iterator as $item) {
     chmod($item, 0777);
 }
 $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('bootstrap/cache'));
-foreach($iterator as $item) {
+foreach ($iterator as $item) {
     chmod($item, 0777);
 }
+
+# #####################################################
 
 echo 'Setting up database...' . PHP_EOL;
 
@@ -65,7 +96,9 @@ chmod(__DIR__ . '/database/test_database.sqlite', 0777);
 chmod(__DIR__ . '/database', 0777);
 echo PHP_EOL;
 
-echo 'Setting up files...' . PHP_EOL;
+# #####################################################
+
+echo 'Setting up configuration files...' . PHP_EOL;
 
 # Web server-related configuration
 copy(
@@ -75,36 +108,47 @@ copy(
 
 # Configuration files
 copy(
-  __DIR__ . '/.env.example',
-  __DIR__ . '/.env'
+    __DIR__ . '/.env.example',
+    __DIR__ . '/.env'
 );
 copy(
     __DIR__ . '/phpunit.xml.example',
     __DIR__ . '/phpunit.xml'
 );
 
-# Language files
-$additionalLanguage = 'it';
+# #####################################################
+
+echo 'Setting up locale files...' . PHP_EOL;
+
 # Function taken from http://www.beliefmedia.com/copy-directory-php
-function beliefmedia_recurse_copy($src, $dst) {
-  $dir = @opendir($src);
-  if (!file_exists($dst)) @mkdir($dst);
-  while (false !== ($file = readdir($dir))) {
-      if (( $file != '.' ) && ( $file != '..' )) {
-         if ( is_dir($src . '/' . $file) ) beliefmedia_recurse_copy($src . '/' . $file, $dst . '/' . $file);
-         else copy($src . '/' . $file, $dst . '/' . $file);
-      }
-  }
-  closedir($dir);
+function beliefmedia_recurse_copy($src, $dst)
+{
+    $dir = @opendir($src);
+    if (!file_exists($dst)) {
+        @mkdir($dst);
+    }
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                beliefmedia_recurse_copy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+    closedir($dir);
 }
-$src = __DIR__ . '/vendor/caouecs/laravel-lang/src/' . $additionalLanguage;
-$dst = __DIR__ . '/resources/lang/' . $additionalLanguage;
+
+$src = $options['locale_source_path'] . '/' . $options['locale'];
+$dst = $options['locale_destination_path'] . '/' . $options['locale'];
 beliefmedia_recurse_copy($src, $dst);
+
+# #####################################################
 
 echo 'Setting up additional software...' . PHP_EOL;
 
 # phpLiteAdmin download
-$file = file_get_contents($urls['phpliteadmin']);
+$file = file_get_contents($options['phpliteadmin_url']);
 file_put_contents(sys_get_temp_dir() . '/phpliteadmin.zip', $file);
 $zip = new ZipArchive();
 if ($zip->open(sys_get_temp_dir() . '/phpliteadmin.zip') === true) {
@@ -117,26 +161,38 @@ copy(
     __DIR__ . '/public/phpliteadmin/phpliteadmin.config.php'
 );
 
+# #####################################################
+
 # Base URL and database file path setting
-echo 'Application URL? (e.g.: http://localhost/clqei) ';
-$applicationUrl = trim(fgets(STDIN));
 $file = file_get_contents(__DIR__ . '/.env');
-$file = preg_replace('#http://localhost#', $applicationUrl, $file);
+$file = preg_replace('#http://localhost#', $options['application_url'], $file);
 $file = preg_replace('#/absolute/path/to/database#', realpath(__DIR__ . '/database'), $file);
 file_put_contents(__DIR__ . '/.env', $file);
+
 $file = file_get_contents(__DIR__ . '/phpunit.xml');
 $file = preg_replace('#/absolute/path/to/database#', realpath(__DIR__ . '/database'), $file);
 file_put_contents(__DIR__ . '/phpunit.xml', $file);
-$baseUrl = (parse_url($applicationUrl, PHP_URL_PATH) === '' ? '/' : parse_url($applicationUrl, PHP_URL_PATH));
+
+$baseUrl = parse_url($options['application_url'], PHP_URL_PATH);
+if ($baseUrl === '') {
+    $baseUrl = '/';
+}
 $file = file_get_contents(__DIR__ . '/public/.htaccess');
 $file = preg_replace('#RewriteBase\s/#', 'RewriteBase ' . $baseUrl, $file);
 file_put_contents(__DIR__ . '/public/.htaccess', $file);
 
+# #####################################################
+
 # Post-install instructions
-echo PHP_EOL;
-echo "Manual steps:" . PHP_EOL;
-echo PHP_EOL;
-echo " - password and cookie name in public/phpliteadmin/phpliteadmin.config.php" . PHP_EOL;
-echo "    - \$password = 'admin'" . PHP_EOL;
-echo "    - \$cookie_name = 'pla3412'" . PHP_EOL;
+if ($options['with_phpliteadmin'] === true) {
+    echo PHP_EOL;
+    echo "Manual steps:" . PHP_EOL;
+    echo PHP_EOL;
+    echo " - password and cookie name in public/phpliteadmin/phpliteadmin.config.php" . PHP_EOL;
+    echo "    - \$password = 'admin'" . PHP_EOL;
+    echo "    - \$cookie_name = 'pla3412'" . PHP_EOL;
+}
+
+# #####################################################
+
 echo PHP_EOL;
