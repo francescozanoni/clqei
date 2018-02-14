2f8ad4b46f210e3e0ac929ca0386cde66ec24e62
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2018-02-14 13:59:37 +0100

    answer table seeder: refactored to reduce cognitive complexity

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -14,42 +14,38 @@ class AnswersTableSeeder extends Seeder
     public function run()
     {
 
-        $questionnaire = $this->getQuestionnaire();
-        
         $currentDateTime = Carbon::now()->format('Y-m-d H:i:s');
-        $questions = [];
 
         $dataToInsert = [];
-        
-        foreach ($questionnaire as $section => $sectionQuestions) {
-            foreach ($sectionQuestions as $question => $answers) {
-                $questions[] = $answers;
-            }
-        }
 
-            foreach ($questions as $questionIndex => $answers) {
-                // Answers other than arrays are free text,
-                // therefore not listed on "answers" table.
-                if (is_array($answers) === true) {
-                    foreach ($answers as $answerIndex => $answer) {
-                        $dataToInsert[] = [
-                            'text' => $answer,
-                            'question_id' => ($questionIndex + 1),
-                            'position' => ($answerIndex + 1),
-                            'created_at' => $currentDateTime,
-                        ];
-                    }
+        foreach ($this->getQuestionAnswers() as $questionId => $answers) {
+            // Answers other than arrays are free text,
+            // therefore not listed on "answers" table.
+            if (is_array($answers) === true) {
+                foreach ($answers as $answerIndex => $answer) {
+                    $dataToInsert[] = [
+                        'text' => $answer,
+                        'question_id' => $questionId,
+                        'position' => ($answerIndex + 1),
+                        'created_at' => $currentDateTime,
+                    ];
                 }
             }
+        }
 
         DB::table('answers')
             ->insert($dataToInsert);
 
     }
-    
-    private function getQuestionnaire()
+
+    /**
+     * Generator of question IDs and the related answers
+     *
+     * @return Generator
+     */
+    private function getQuestionAnswers()
     {
-        return
+        $questionnaire =
             json_decode(
                 file_get_contents(
                     database_path(
@@ -59,6 +55,14 @@ class AnswersTableSeeder extends Seeder
                 true
             );
 
+        $questionId = 1;
+
+        foreach ($questionnaire as $section => $sectionQuestions) {
+            foreach ($sectionQuestions as $question => $answers) {
+                yield $questionId++ => $answers;
+            }
+        }
+
     }
-    
+
 }
