15b2df95ddaaa6ea7d0984e0492ff2b9cb92b2a8
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2018-03-22 14:17:20 +0100

    commands: added test e-mail delivery command

---
+++ app/Console/Commands/SendTestMail.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Console\Commands;
+
+use App;
+use App\Console\Commands\Interfaces\WithIntegerExitCode;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class SendTestMail extends Command implements WithIntegerExitCode
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:mail
+                            {recipient: recipient e-mail address}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send a test e-mail';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int exit code
+     */
+    public function handle() : int
+    {
+
+        $recipient = $this->argument('recipient');
+
+        try {
+
+            Mail::to($recipient)
+                ->send(new App\Mail\Test());
+
+        } catch (\Throwable $e) {
+
+            $this->info('E-mail delivery failed: ' . $e->getMessage());
+            return self::UNEXPECTED_ERROR;
+
+        }
+
+        $this->info('E-mail successfully sent');
+
+        return self::SUCCESS;
+
+    }
+
+}
---
+++ app/Mail/Test.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class Test extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     */
+    public function build()
+    {
+        return $this->view('emails.test');
+    }
+
+}
