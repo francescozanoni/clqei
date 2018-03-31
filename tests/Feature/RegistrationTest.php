f2aea4bc4d21d80a433a92c25a670fa280d84cc0
Francesco Zanoni <francescozanoni81@gmail.com>
2018-03-31 09:25:15 +0200

    tests: started registration test

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Feature;
+
+use App;
+use App\Observers\EloquentModelObserver;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * Successful creation.
+     */
+    public function testSuccess()
+    {
+
+        $response =
+            $this->post(
+                    route(
+                        'register',
+                        [
+                            'first_name' => 'Foo',
+                            'last_name' => 'Bar',
+                            'email' => 'foo.bar@example.com',
+                            'password' => 'password',
+                            'password_confirmation' => 'password',
+                            'role' => \App\User::ROLE_STUDENT,
+                            'identification_number' => '12121212',
+                            'nationality' => 'IT',
+                            'gender' => 'male',
+                        ]
+                    )
+                );
+
+        $response->assertRedirect(route('home'));
+        /*
+        $response->assertSessionHas(EloquentModelObserver::FLASH_MESSAGE_KEY,
+            __('The new compilation has been created'));
+            */
+        $this->assertDatabaseHas('users', ['id' => 1]);
+        $this->assertDatabaseMissing('users', ['id' => 2]);
+        $this->assertDatabaseHas('students', ['id' => 1]);
+        $this->assertDatabaseMissing('students', ['id' => 2]);
+
+    }
+    
+}
