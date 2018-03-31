05688e777d5b244e25a5cbe0485fb6ecbe74b64d
Francesco Zanoni <francescozanoni81@gmail.com>
2018-03-31 16:02:27 +0200

    registration test: added duplicate student test

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -76,10 +76,101 @@ class RegistrationTest extends TestCase
                     );
 
             $response->assertSessionHasErrors([$keyToRemove]);
+            
             $this->assertDatabaseMissing('users', ['id' => 1]);
             $this->assertDatabaseMissing('students', ['id' => 1]);
+            
+            $this->assertGuest();
 
         }
+
+    }
+    
+    /**
+     * Failed registration: student already registered.
+     */
+    public function testStudentAlreadyRegistered()
+    {
+    
+        $this->seed();
+
+        // Both e-mail and identification number duplicate.
+        $response =
+            $this->post(
+                    route(
+                        'register',
+                        [
+            'first_name' => 'Example',
+            'last_name' => 'Student',
+            'email' => 'student@example.com',
+            'password' => 'student',
+            'password_confirmation' => 'student',
+            'role' => User::ROLE_STUDENT,
+            'identification_number' => '12345678',
+            'nationality' => 'IT',
+            'gender' => 'male',
+        ]
+                    )
+                );
+
+        $response->assertSessionHasErrors(['email', 'identification_number']);
+        
+        $this->assertDatabaseMissing('users', ['id' => 4]);
+        $this->assertDatabaseMissing('students', ['id' => 2]);
+        
+        $this->assertGuest();
+        
+        // Duplicate e-mail.
+        $response =
+            $this->post(
+                    route(
+                        'register',
+                        [
+            'first_name' => 'Example',
+            'last_name' => 'Student',
+            'email' => 'student@example.com',
+            'password' => 'student',
+            'password_confirmation' => 'student',
+            'role' => User::ROLE_STUDENT,
+            'identification_number' => '12121212',
+            'nationality' => 'IT',
+            'gender' => 'male',
+        ]
+                    )
+                );
+
+        $response->assertSessionHasErrors(['email']);
+        
+        $this->assertDatabaseMissing('users', ['id' => 4]);
+        $this->assertDatabaseMissing('students', ['id' => 2]);
+        
+        $this->assertGuest();
+        
+        // Duplicate identification number.
+        $response =
+            $this->post(
+                    route(
+                        'register',
+                        [
+            'first_name' => 'Example',
+            'last_name' => 'Student',
+            'email' => 'another.student@example.com',
+            'password' => 'student',
+            'password_confirmation' => 'student',
+            'role' => User::ROLE_STUDENT,
+            'identification_number' => '12345678',
+            'nationality' => 'IT',
+            'gender' => 'male',
+        ]
+                    )
+                );
+
+        $response->assertSessionHasErrors(['identification_number']);
+        
+        $this->assertDatabaseMissing('users', ['id' => 4]);
+        $this->assertDatabaseMissing('students', ['id' => 2]);
+        
+        $this->assertGuest();
 
     }
     
