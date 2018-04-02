023623f496ee1586fcf053c5c345823d6dd1cb0e
Francesco Zanoni <francescozanoni81@gmail.com>
2018-04-02 14:56:19 +0200

    tests: added empty administrator and viewer creation tests

---
+++ tests/Feature/AdministratorCreationTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Feature;
+
+use App;
+use App\Observers\EloquentModelObserver;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AdministratorCreationTest extends TestCase
+{
+
+}
