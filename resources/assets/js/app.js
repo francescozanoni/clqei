63cbe5f2c74b50b801d515b31a9fcab84a14446d
Francesco Zanoni <francescozanoni81@gmail.com>
2018-04-07 13:36:03 +0200

    disabled submit button after form submit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,3 +5,8 @@
  */
 
 require('./bootstrap');
+
+// https://stackoverflow.com/questions/5691054/disable-submit-button-on-form-submit/5691065
+$('form').submit(function () {
+    $(this).find(':input[type=submit]').prop('disabled', true);
+});
