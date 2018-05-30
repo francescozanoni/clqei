eafdcd41bcbed3287556a46e9b68c8ce260d1c6f
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2018-05-30 15:43:21 +0200

    country service: added comments

--- app/Services/CountryService.php
+++ app/Services/CountryService.php
@@ -16,11 +16,26 @@ class CountryService
         $this->countries = $countries;
     }
 
+    /**
+     * Get all localized country names, indexed by ISO country codes.
+     *
+     * @return array e.g. Array (
+     *                      [AF] => Afghanistan
+     *                      [AL] => Albania
+     *                      [DZ] => Algeria
+     *                      [...]
+     *                    )
+     */
     public function getCountries() : array
     {
         return $this->countries;
     }
 
+    /**
+     * Get all ISO country codes.
+     *
+     * @return array
+     */
     public function getCountryCodes() : array
     {
         return array_keys($this->countries);
