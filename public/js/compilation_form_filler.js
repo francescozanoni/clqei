b6d04e8296e31148f779ee4d634ff772724035db
Francesco Zanoni <francescozanoni81@gmail.com>
2018-08-05 15:31:51 +0200

    compilation form automatic filler: added date field management

--- public/js/compilation_form_filler.js
+++ public/js/compilation_form_filler.js
@@ -33,9 +33,41 @@ function fillForm() {
             var options = form.find('input[type=checkbox][name="' + this.name + '"]');
             options[Math.floor(Math.random() * options.length)].checked = true;
         });
+        
+        // Random compilation of date fields.
+        form.find('input[type=date]').each(function () {
+            var fromDate = nDaysAgoDate(60);
+            var toDate = (new Date()).toISOString().substring(0, 10); // today
+            $(this).val(randomDate(fromDate, toDate));
+        });
 
     } catch (e) {
         alert(e);
     }
 
+}
+
+/**
+ * @see http://jsfiddle.net/t94778xt/
+ */
+function randomDate(fromDate, toDate) {
+
+    var from_ts = new Date(fromDate).getTime();
+    var to_ts   = new Date(toDate).getTime();
+    var fDate   = new Date(Math.floor(Math.random() * (to_ts - from_ts)) + from_ts);
+    
+    return fDate.toISOString().substring(0, 10);
+    
+}
+
+/**
+ * @see https://community.apigee.com/questions/51354/how-to-write-a-javascript-for-subtracting-days-fro.html
+ */
+function nDaysAgoDate(days) {
+
+    var myDate = new Date();
+    myDate.setDate(myDate.getDate() - days);
+    
+    return myDate.toISOString().substring(0, 10);
+    
 }
