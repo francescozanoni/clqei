70102485de284ca85f009d371ddddef756ec18d3
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-12-17 09:24:40 +0100

    [FIX] tests: aligned namespaces

--- tests/Feature/AccessiblePages/AdministratorTest.php
+++ tests/Feature/AccessiblePages/AdministratorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace Tests\Feature;
+namespace Tests\Feature\AccessiblePages;
 
 use App\Models\Location;
 use App\Models\Ward;
--- tests/Feature/AccessiblePages/ViewerTest.php
+++ tests/Feature/AccessiblePages/ViewerTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace Tests\Feature;
+namespace Tests\Feature\AccessiblePages;
 
 use App\Models\Location;
 use App\Models\Ward;
